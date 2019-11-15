af4f9e94a53fea631f71783ec06667c2bb4d2303
”Mina” <minaa.hosseinii@gmail.com>
2019-11-15 03:00:27 +0100

    dialog class added

--- app/src/main/java/com/company/betterme/DialogAdd.java
+++ app/src/main/java/com/company/betterme/DialogAdd.java
@@ -1,4 +1,72 @@
 package com.company.betterme;
 
-public class DialogAdd {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class DialogAdd extends DialogFragment {
+
+    private ImageButton mBtnClose;
+    private EditText mInputWhat;
+    private DatePicker mInputWhen;
+    private Button mBtnAdd;
+
+    //empty constructor
+    public DialogAdd() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog_add, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mBtnClose = view.findViewById(R.id.btn_close);
+        mInputWhat = view.findViewById(R.id.et_add);
+        mInputWhen = view.findViewById(R.id.date_picker);
+        mBtnAdd = view.findViewById(R.id.btn_add);
+
+        mBtnClose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                    dismiss();
+                                           }
+                });
+
+        mBtnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addAction();
+
+            }
+        });
+
+    }
+
+    private void addAction() {
+
+        //get user's input
+        String what = mInputWhat.getText().toString();
+
+        //get the time
+        long now = System.currentTimeMillis();
+
+        //add these two objects to an object
+
+
+    }
+
+
 }
--- app/src/main/java/com/company/betterme/MainActivity.java
+++ app/src/main/java/com/company/betterme/MainActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import  android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity  {
 
@@ -21,8 +24,7 @@ public class MainActivity extends AppCompatActivity  {
         mBtnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Button was clicked", Toast.LENGTH_SHORT).show();
-
+                showDialogAdd();
             }
         });
 
@@ -30,8 +32,26 @@ public class MainActivity extends AppCompatActivity  {
         setSupportActionBar(mToolbar);
         mToolbar.setTitleTextColor(0xFFFFFFFF);
 
+        //Addjust the image by Glide library so that it can be shown perfectly on any device
+        initBackgroundImage();
+
     }
 
+    private void showDialogAdd() {
+        //create an object of dialog
+        DialogAdd dialog = new DialogAdd();
+        //show the dialog by passing fragment manager
+        dialog.show(getSupportFragmentManager(), "Add");
+
+    }
+
+    private void initBackgroundImage(){
+        ImageView background = findViewById(R.id.iv_background);
+        Glide.with(this)
+                .load(R.drawable.background)
+                .centerCrop()
+                .into(background);
+    }
 
 
 }
